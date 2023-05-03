f9444eefc17d0e469c55c1a7c895123c9c02f0a3
Pavel Burgr <pb@drake.cz>
2023-05-03 22:50:30 +0200

    WIP

--- fw/app/main.py
+++ fw/app/main.py
@@ -3,15 +3,18 @@ import mqtt_reg
 
 import sys
 sys.path.append('/')
-
 import site_config
+
 
 print('48V battery monitor')
 
+voltage = mqtt_reg.ServerReadOnlyRegister(site_config.name + '.voltage', {'device': site_config.name, 'unit': 'V', 'type': 'number'})
+current = mqtt_reg.ServerReadOnlyRegister(site_config.name + '.current', {'device': site_config.name, 'unit': 'A', 'type': 'number'})
+power = mqtt_reg.ServerReadOnlyRegister(site_config.name + '.power', {'device': site_config.name, 'unit': 'W', 'type': 'number'})
+temp = mqtt_reg.ServerReadOnlyRegister(site_config.name + '.temp', {'device': site_config.name, 'unit': '°C', 'type': 'number'})
+
 registry = mqtt_reg.Registry(
-    server=[
-        # mqtt_reg.ServerRegister('A', {'gogo': 'logo'})
-    ],
+    server=[voltage, current, power],
     wifi_ssid=site_config.wifi_ssid,
     wifi_password=site_config.wifi_password,
     mqtt_broker=site_config.mqtt_broker,
@@ -19,5 +22,4 @@ registry = mqtt_reg.Registry(
     debug=site_config.debug
 )
 
-# registry.start()
-
+registry.start()
--- fw/site_config.py
+++ fw/site_config.py
@@ -2,3 +2,4 @@ wifi_ssid='416T'
 wifi_password='ferdamravenec'
 mqtt_broker='10.1.0.143'
 debug=True
+name='pv.battery'
